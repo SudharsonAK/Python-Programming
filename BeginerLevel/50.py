fece15027b66282a9bf705c8fd982610bc6ed48b
SudharsonAK <34906460+SudharsonAK@users.noreply.github.com>
2018-02-23 12:03:37 +0530

    Add files via upload

---
+++ BeginerLevel/50.py
@@ -0,0 +1,25 @@
+
+
+    if(a == 0):
+
+        return False
+
+    while (a!=1):
+
+            if((a%2)!=0):
+
+                return False
+
+            a=a//2
+
+    return True
+
+n=input()
+
+if(pot(n)):
+
+    print("yes")
+
+else:
+
+    print("no")
