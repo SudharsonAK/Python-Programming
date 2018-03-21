6f592ecaee748dbe0d88bcde8d6b29a4ccb98334
SudharsonAK <34906460+SudharsonAK@users.noreply.github.com>
2018-03-21 16:59:01 +0530

    Update 11.py

--- PlayerLevel/11.py
+++ PlayerLevel/11.py
@@ -2,8 +2,8 @@ b=str(input())
 
   if(b=='SUNDAY' or b=='Sunday' or b=='sunday'or b=='SATURDAY' or b=='Saturday'or b=='saturday'):
 
-      print("Holiday")
+      print("Yes")
 
   else:
 
-      print("Working day")
+      print("no")
