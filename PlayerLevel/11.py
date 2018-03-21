0960d4658f20fc37fb972fac9e67c563db9809b8
SudharsonAK <34906460+SudharsonAK@users.noreply.github.com>
2018-03-21 16:57:27 +0530

    Create 11.py

---
+++ PlayerLevel/11.py
@@ -0,0 +1,9 @@
+b=str(input())
+
+  if(b=='SUNDAY' or b=='Sunday' or b=='sunday'or b=='SATURDAY' or b=='Saturday'or b=='saturday'):
+
+      print("Holiday")
+
+  else:
+
+      print("Working day")
