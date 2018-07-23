1c638a372187db4769abfa9a849cc4aeda0605b1
SudharsonAK <34906460+SudharsonAK@users.noreply.github.com>
2018-07-23 10:49:01 +0530

    Update 1.py

--- pro/1.py
+++ pro/1.py
@@ -1,8 +1,8 @@
-a=int(input("\nEnter the number:"))
-b=input("\nEnter the string:")
+n=int(input("\nEnter the number:"))
+m=input("\nEnter the string:")
 c=list(b)
 d=0
 while a>0:
     print(c[d],end="")
     d=d+1
-    a=a-1
+    n=n-1
